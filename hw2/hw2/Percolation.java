b5a3c6c0721c10fece2ffa9949c5f974d6d42894
Badwoman <lianyukang@foxmail.com>
2022-06-05 21:00:19 +0800

    Finish HW2 Interface

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,39 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    /** * create N-by-N grid, with all sites initially blocked */
+    public Percolation(int N){
+
+    }
+
+    /** * open the site (row, col) if it is not open already */
+    void open(int row,int col){
+
+    }
+
+    /** * is the site (row, col) open? */
+    boolean isOpen(int row,int col){
+        return true;
+    }
+
+    /** * is the site (row, col) full? */
+    boolean isFull(int row,int col){
+        return true;
+    }
+
+    /** * number of open sites */
+    int numberOfOpenSites(){
+        return 0;
+    }
+
+    /** * does the system percolate? */
+    boolean percolates(){
+        return true;
+    }
+
+    /** * use for unit testing (not required) */
+    static void main(String[] args){
+
+    }
+
 }
