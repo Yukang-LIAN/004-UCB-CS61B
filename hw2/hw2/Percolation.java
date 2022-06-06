f5026f22e34906eb6fe96f4832e4d4f146db0e99
Badwoman <lianyukang@foxmail.com>
2022-06-06 17:24:30 +0800

    Finish HW2 Percolation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,40 +2,116 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.ArrayList;
+
 public class Percolation {
 
-    /** * create N-by-N grid, with all sites initially blocked */
-    public Percolation(int N){
+    private final int N;
+    private int openNum = 0;
+    private int topSite;
+    private int bottomSite;
+    private boolean openFlag[][];
+    private WeightedQuickUnionUF model;
 
+    /**
+     * * create N-by-N grid, with all sites initially blocked
+     */
+    public Percolation(int N) {
+        if (N < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.N = N;
+        this.topSite = N * N;
+        this.bottomSite = N * N + 1;
+        openFlag = new boolean[N][N];
+        model = new WeightedQuickUnionUF(N * N + 2);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                openFlag[i][j] = false;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            model.union(topSite, xyTo1D(0, i));
+        }
+
+        for (int i = 0; i < N; i++) {
+            model.union(bottomSite, xyTo1D(N - 1, i));
+        }
     }
 
-    /** * open the site (row, col) if it is not open already */
-    void open(int row,int col){
-
+    /**
+     * open the site (row, col) if it is not open already
+     */
+    void open(int row, int col) {
+        if (isOpen(row, col)) {
+            return;
+        }
+        openFlag[row][col] = true;
+        openNum++;
+        connectWithNeighbor(row, col);
     }
 
-    /** * is the site (row, col) open? */
-    boolean isOpen(int row,int col){
-        return true;
+    /**
+     * is the site (row, col) open?
+     */
+    boolean isOpen(int row, int col) {
+        return openFlag[row][col];
     }
 
-    /** * is the site (row, col) full? */
-    boolean isFull(int row,int col){
-        return true;
+    /**
+     * is the site (row, col) full?
+     */
+    boolean isFull(int row, int col) {
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        return model.connected(xyTo1D(row, col), topSite);
     }
 
-    /** * number of open sites */
-    int numberOfOpenSites(){
-        return 0;
+    /**
+     * number of open sites
+     */
+    int numberOfOpenSites() {
+        return openNum;
     }
 
-    /** * does the system percolate? */
-    boolean percolates(){
-        return true;
+    /**
+     * does the system percolate?
+     */
+    boolean percolates() {
+        return model.connected(topSite, bottomSite);
     }
 
-    /** * use for unit testing (not required) */
-    static void main(String[] args){
+    /**
+     * * return x y to an array
+     */
+    private int xyTo1D(int r, int c) {
+        return r * this.N + c;
+    }
+
+    private void connectWithNeighbor(int row, int col) {
+        connectBlock(row, col, row - 1, col);
+        connectBlock(row, col, row + 1, col);
+        connectBlock(row, col, row, col + 1);
+        connectBlock(row, col, row, col - 1);
+    }
+
+    private void connectBlock(int ax, int ay, int bx, int by) {
+        if (bx < 0 || bx >= N || by < 0 || by >= N) {
+            return;
+        }
+        if (isOpen(bx, by)) {
+            model.union(xyTo1D(ax, ay), xyTo1D(bx, by));
+        }
+    }
+
+    /**
+     * use for unit testing (not required)
+     */
+    static void main(String[] args) {
 
     }
 
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -4,7 +4,9 @@ public class PercolationStats {
 
     /** * perform T independent experiments on an N-by-N grid */
     public PercolationStats(int N, int T, PercolationFactory pf){
-
+        if(N<=0||T<=0){
+            throw new IllegalArgumentException();
+        }
     }
 
     /** * sample mean of percolation threshold */
